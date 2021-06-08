3231e8a7b852f89084bcf8629243c1367b884b6b
A S Lewis <aslewis@cpan.org>
2021-06-08 20:07:46 +0100

    Add 35 new crafting materials

--- README.rst
+++ README.rst
@@ -2,16 +2,16 @@
 decoshelves
 ===========
 
-This is a mod for `Minetest <https://www.minetest.net/>`__, providing an unlimited selection of bookshelves.
+This is a mod for `Minetest <https://www.minetest.net/>`__, providing an expanded selection of bookshelves.
 
 .. image:: screenshots/decoshelves.png
   :alt: A selection of bookshelves
 
-Have YOU ever wanted to make a bookshelf out of diamond blocks, leaves, coral, or any other completely unrealistic material? Well, now you can!
+Have YOU ever wanted to make a bookshelf out of leaves, coral, lava or any other completely unrealistic material? Well, now you can!
 
-The default mod from `minetest-game <https://github.com/minetest/minetest_game/>`__ provides a single bookshelf made out of default wood.
+`minetest-game <https://github.com/minetest/minetest_game/>`__ provides a single bookshelf made out of default wood.
 
-This mod provides (as standard) bookshelves made out of acacia, aspen, jungle and pine wood, as well as one made out of stone.
+This mod provides (as standard) bookshelves made out of forty additional materials.
 
 You can easily add or remove bookshelves; just open the init.lua file in a text editor, and add or remove lines from the list. You can create a bookshelf out of (almost) any material: as long as minetest can retrieve the texture, than decoshelves can use that texture.
 
@@ -21,3 +21,4 @@ License (media): Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)
 Credits: The code and textures are adapted from `minetest-game <https://github.com/minetest/minetest_game/>`__.
 
 Dependencies: default
+Optional dependencies: bones and bucket
--- init.lua
+++ init.lua
@@ -2,6 +2,8 @@
 -- decoshelves mod by A S Lewis, based on code from minetest-game/default
 -- License (code): GNU Lesser General Public License, version 2.1
 -- License (media): Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)
+--
+-- Literally cobbled together during a lunchbreak. Don't expect the code to be elegant
 ---------------------------------------------------------------------------------------------------
 
 local S = minetest.get_translator(minetest.get_current_modname())
@@ -20,26 +22,111 @@ decoshelves.name = "decoshelves"
 
 decoshelves.ver_max = 1
 decoshelves.ver_min = 0
-decoshelves.ver_rev = 0
+decoshelves.ver_rev = 1
 
 ---------------------------------------------------------------------------------------------------
 -- Customisation
 ---------------------------------------------------------------------------------------------------
 
--- You can make a bookshelf out of any node you like!
+-- You can make a new bookshelf out of any node you like, or using any texture you like!
 -- Add or remove as many lines as you like from this list
--- Just make sure that the parent mod has already been loaded, by adding that mod to mod.conf
+-- If you specify a texture directly, it must be added to this mod's /textures folder
+-- If you specify a node from another mod, add that mod to this mod's mod.conf file
+-- Note that decoshelves can't usually retrieve textures from water and other specialised nodes
+
 setup_list = {
-    "default:aspen_wood",       -- The node whose texture we'll borrow
-    "aspen",                    -- This word is used to name the new bookshelf node
-    S("Aspen Wood Bookshelf"),  -- The new bookshelf node's description
-    --
-    "default:wood", "apple", S("Apple Tree Wood Bookshelf"),
-    "default:acacia_wood", "acacia", S("Acacia Wood Bookshelf"),
-    "default:junglewood", "jungle", S("Jungle Wood Bookshelf"),
-    "default:pine_wood", "pine", S("Pine Wood Bookshelf"),
-    "default:stone", "stone", S("Stone Bookshelf"),
+    -- The node whose texture we'll borrow. You can also specify the texture directly, if you have
+    --      copied the texture into this mod's /textures folder
+    "default:aspen_wood",
+    -- The node used to craft the bookshelf. If it's the same as the node above, you can use an
+    --      empty string
+    "",
+    -- This word is used to name the new bookshelf node
+    "aspen",
+    -- This description is added to the new bookshelf node
+    S("Aspen Wood Bookshelf"),
+
+    -- Add your own bookshelves here!
+    "default:aspen_tree", "", "aspen_trunk", S("Aspen Tree Bookshelf"),
+    "default:aspen_leaves", "", "aspen_leaves", S("Aspen Leaf Bookshelf"),
+
+    "default:tree", "", "apple_trunk", S("Apple Tree Bookshelf"),
+    "default:wood", "", "apple_wood", S("Apple Wood Bookshelf"),
+    "default:leaves", "", "apple_leaves", S("Apple Leaf Bookshelf"),
+
+    "default:acacia_tree", "", "acacia_trunk", S("Acacia Tree Bookshelf"),
+    "default:acacia_wood", "", "acacia_wood", S("Acacia Wood Bookshelf"),
+    "default:acacia_leaves", "", "acacia_leaves", S("Acacia Leaf Bookshelf"),
+
+    "default:jungletree", "", "jungle_trunk", S("Jungle Tree Bookshelf"),
+    "default:junglewood", "", "jungle_wood", S("Jungle Wood Bookshelf"),
+    "default:jungleleaves", "", "jungle_leaves", S("Jungle Leaf Bookshelf"),
+
+    "default:pine_tree", "", "pine_trunk", S("Pine Tree Bookshelf"),
+    "default:pine_wood", "", "pine_wood", S("Pine Wood Bookshelf"),
+    "default:pine_needles", "", "pine_needles", S("Pine Needle Bookshelf"),
+
+    "default:stone", "", "stone", S("Stone Bookshelf"),
+    "default:cobble", "", "cobble", S("Coblestone Bookshelf"),
+
+    "default:desert_stone", "", "desert_stone", S("Desert Stone Bookshelf"),
+    "default:desert_cobble", "", "desert_cobble", S("Desert Cobbletone Bookshelf"),
+
+    "default:sandstone", "", "sandstone", S("Sandstone Bookshelf"),
+    "default:sand", "", "sand", S("Sand Bookshelf"),
+
+    "default:desert_sandstone", "", "desert_sandstone", S("Desert Sandstone Bookshelf"),
+    "default:desert_sand", "", "desert_sand", S("Desert Sand Bookshelf"),
+
+    "default:silver_sandstone", "", "silver_sandstone", S("Silver Sandstone Bookshelf"),
+    "default:silver_sand", "", "silver_sand", S("Silver Sand Bookshelf"),
+
+    "default:cactus", "", "cactus", S("Cactus Bookshelf"),
+
+    "default:glass", "", "glass", S("Glass Bookshelf"),
+    "default:obsidian", "", "obsidian", S("Obsidian Bookshelf"),
+    "default:obsidian_glass", "", "obsidian_glass", S("Obsidian Glass Bookshelf"),
+
+    "default:coral_brown", "", "coral_brown", S("Brown Coral Bookshelf"),
+    "default:coral_orange", "", "coral_orange", S("Orange Coral Bookshelf"),
+
+    "default:dirt", "", "dirt", S("Dirt Bookshelf"),
+    "default:dirt_with_grass", "", "dirt_grass", S("Grass Bookshelf"),
+    "default:dirt_with_dry_grass", "", "dirt_dry_grass", S("Dry Grass Bookshelf"),
+
+    "default:gravel", "", "gravel", S("Gravel Bookshelf"),
+    "default:ice", "", "ice", S("Ice Bookshelf"),
+    "default:mese", "", "mese", S("Mese Bookshelf"),
+
+--    "decoshelves_water.png", "default:water_source", "water", S("Wet Bookshelf"),
+--    "decoshelves_river_water.png", "", "river_water", S("Watery Bookshelf"),
+--    "decoshelves_lava.png", "", "lava", S("Lava Bookshelf"),
 }
+
+-- (default is a hard dependency, but bones and bucket are soft dependencies)
+if minetest.get_modpath("bones") then
+    table.insert(setup_list, "bones:bones")
+    table.insert(setup_list, "")
+    table.insert(setup_list, "bones")
+    table.insert(setup_list, "Bony bookshelf")
+end
+
+if minetest.get_modpath("bucket") then
+    table.insert(setup_list, "decoshelves_water.png")
+    table.insert(setup_list, "bucket:bucket_water")
+    table.insert(setup_list, "water")
+    table.insert(setup_list, "Wet bookshelf")
+
+    table.insert(setup_list, "decoshelves_river_water.png")
+    table.insert(setup_list, "bucket:bucket_river_water")
+    table.insert(setup_list, "river_water")
+    table.insert(setup_list, "River bookshelf")
+
+    table.insert(setup_list, "decoshelves_lava.png")
+    table.insert(setup_list, "bucket:bucket_lava")
+    table.insert(setup_list, "lava")
+    table.insert(setup_list, "Lava bookshelf")
+end
 
 ---------------------------------------------------------------------------------------------------
 -- Functions
@@ -99,11 +186,39 @@ local function update_bookshelf(pos)
 
 end
 
-function decoshelves.register_bookshelf(material_node, material_name, descrip)
+function decoshelves.register_bookshelf(material_node, craft_node, material_name, descrip)
 
-    local tile_table = minetest.registered_nodes[material_node].tiles
-    local wood_img = tile_table[1]
-    local shelf_img = wood_img .. "^decoshelves_overlay.png"
+    local tile_table, wood_img, shelf_img
+
+    if string.find(material_node, ".png$") then
+
+        -- The texture has been specified directly
+        wood_img = material_node
+        if craft_node == "" then
+            -- No crafting recipe supplied, so ignore this bookshelf
+            minetest.log("[DECOSHELVES] Invalid bookshelf definition")
+            return
+        end
+
+    elseif minetest.registered_nodes[material_node] == nil then
+
+        minetest.log("[DECOSHELVES] Cannot retrieve texture for node: "..material_node)
+        return
+
+    else
+
+        -- Get the texture from the specified node
+        tile_table = minetest.registered_nodes[material_node].tiles
+        wood_img = tile_table[1]
+
+        if craft_node == "" then
+            -- If the first two arguments are (some_png, nil), then an error will be produced
+            craft_node = material_node
+        end
+
+    end
+
+    shelf_img = wood_img .. "^decoshelves_overlay.png"
 
     minetest.register_node("decoshelves:bookshelf_"..material_name, {
         description = descrip,
@@ -165,9 +280,9 @@ function decoshelves.register_bookshelf(material_node, material_name, descrip)
     minetest.register_craft({
         output = "decoshelves:bookshelf_"..material_name,
         recipe = {
-            {material_node, material_node, material_node},
+            {craft_node, craft_node, craft_node},
             {"default:book", "default:book", "default:book"},
-            {material_node, material_node, material_node},
+            {craft_node, craft_node, craft_node},
         }
     })
 
@@ -178,16 +293,18 @@ end
 ---------------------------------------------------------------------------------------------------
 
 local stop_flag = false
+
 while not stop_flag do
 
     material_node = table.remove(setup_list, 1)
+    craft_node = table.remove(setup_list, 1)
     material_name = table.remove(setup_list, 1)
     descrip = table.remove(setup_list, 1)
 
     if material_node == nil then
         stop_flag = true
     else
-        decoshelves.register_bookshelf(material_node, material_name, descrip)
+        decoshelves.register_bookshelf(material_node, craft_node, material_name, descrip)
     end
 
 end
